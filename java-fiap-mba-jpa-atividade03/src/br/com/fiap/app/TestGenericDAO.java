26240f96d30dc6b9d2490aa60f2df077ce0f844e
logonrm <logonrm@L2704MICRO20.fiap.com.br>
2017-06-12 21:38:32 -0300

    Commit final 2

--- java-fiap-mba-jpa-atividade03/src/br/com/fiap/app/TestGenericDAO.java
+++ java-fiap-mba-jpa-atividade03/src/br/com/fiap/app/TestGenericDAO.java
@@ -22,28 +22,9 @@ public class TestGenericDAO {
 
 	private static void deletarCliente() {
 		Clientes cliente = new Clientes();
-		cliente.setEmail("lramosouza@outlook.com");
-		cliente.setNome("Leandro Ramo");
-
-		Pedidos pedido = new Pedidos();
-		pedido.setCliente(cliente);
-		pedido.setData(new Date());
-		pedido.setDescricao("Galaxy note 8");
-		pedido.setValor(5000);
-
-		cliente.getPedidos().add(pedido);
-
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.MINUTE, -10);
-
-		Pedidos pedido2 = new Pedidos();
-		pedido2.setCliente(cliente);
-		pedido2.setData(cal.getTime());
-		pedido2.setDescricao("Drone");
-		pedido2.setValor(6000);
-
-		cliente.getPedidos().add(pedido2);
-		
+		cliente.setIdCliente(1);
+		Pedidos pedidos = new Pedidos();
+		pedidos.setIdCliente(1);
 		try {
 			GenericDao<Clientes> dao = new GenericDao<>(Clientes.class);
 			dao.remover(cliente);
@@ -57,28 +38,15 @@ public class TestGenericDAO {
 
 	private static void alterarCliente() {
 		Clientes cliente = new Clientes();
-		cliente.setEmail("lramosouza@outlook.com");
-		cliente.setNome("Leandro Ramo");
-
-		Pedidos pedido = new Pedidos();
-		pedido.setCliente(cliente);
-		pedido.setData(new Date());
-		pedido.setDescricao("Galaxy note 8");
-		pedido.setValor(5000);
-
-		cliente.getPedidos().add(pedido);
-
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.MINUTE, -10);
-
-		Pedidos pedido2 = new Pedidos();
-		pedido2.setCliente(cliente);
-		pedido2.setData(cal.getTime());
-		pedido2.setDescricao("Drone");
-		pedido2.setValor(6000);
-
-		cliente.getPedidos().add(pedido2);
-		
+		cliente.setIdCliente(1);
+		cliente.setEmail("lll@outlook.com");
+		cliente.setNome("Leandro");
+		Pedidos pedidos = new Pedidos();
+		pedidos.setCliente(cliente);
+		pedidos.setData(new Date());
+		pedidos.setDescricao("Alterado");
+		pedidos.setIdCliente(1);
+		pedidos.setValor(150);
 		try {
 			GenericDao<Clientes> dao = new GenericDao<>(Clientes.class);
 			dao.atualizar(cliente);
